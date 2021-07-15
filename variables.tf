f797f37a11dafe5db04bd32e3186ab21ba14bc40
Sam Marshall <sam.marshall@cloudscene.com>
2021-07-15 22:24:47 +1000

    docs(variables): improved input variable descriptions

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "name" {
   type        = string
-  description = "Identifier for the group of logs to be partitioned"
+  description = "Identifier for the group of logs to be partitioned."
 }
 
 variable "partitions" {
@@ -9,7 +9,7 @@ variable "partitions" {
     table    = string
     location = string
   }))
-  description = "Map of Athena partitions to be managed"
+  description = "Map of Athena ALB/ELB log partitions to be managed."
 }
 
 variable "query_result" {
@@ -18,11 +18,11 @@ variable "query_result" {
     location   = string
   })
   default     = null
-  description = "Specify the S3 bucket query result location"
+  description = "Specify an existing (S3 bucket) query result location. If not set, the module will create an S3 bucket for the functions query results."
 }
 
 variable "tags" {
   type        = map(string)
   default     = {}
-  description = "AWS resource tags"
+  description = "AWS resource tags."
 }
