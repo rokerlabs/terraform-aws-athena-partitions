a88076e777b05a93256d87644408cf6222a7e34a
Sam Marshall <sam.marshall@cloudscene.com>
2021-07-15 19:52:25 +1000

    refactor(artifact download): always download the athena-partitions artifact

--- lambda.tf
+++ lambda.tf
@@ -1,8 +1,6 @@
 resource "null_resource" "this" {
   triggers = {
-    artifact       = local.package_file
-    config         = local.lambda_config
-    module_version = "2.7.0"
+    always_run = timestamp()
   }
 
   provisioner "local-exec" {
@@ -14,17 +12,30 @@ module "lambda" {
   source  = "terraform-aws-modules/lambda/aws"
   version = "2.7.0"
 
-  function_name                           = local.lambda_config.function_name
-  handler                                 = local.lambda_config.handler
-  runtime                                 = local.lambda_config.runtime
-  create_package                          = local.lambda_config.create_package
-  local_existing_package                  = local.lambda_config.local_existing_package
-  attach_policy                           = local.lambda_config.attach_policy
-  policy                                  = local.lambda_config.policy
-  memory_size                             = local.lambda_config.memory_size
-  allowed_triggers                        = local.lambda_config.allowed_triggers
-  create_current_version_allowed_triggers = local.lambda_config.create_current_version_allowed_triggers
-  tags                                    = local.lambda_config.tags
+  function_name = local.name
+  handler       = local.name
+  runtime       = "go1.x"
+
+  create_package         = false
+  local_existing_package = local.package_file
+
+  attach_policy = true
+  policy        = aws_iam_policy.this.arn
+
+  memory_size = 128
+
+  allowed_triggers = {
+    CloudWatchEvent = {
+      principal  = "events.amazonaws.com"
+      source_arn = aws_cloudwatch_event_rule.this.arn
+    }
+  }
+  # Disable creating trigger permission for published versions, since publish = false.
+  create_current_version_allowed_triggers = false
+
+  tags = merge(var.tags, {
+    Name = local.name
+  })
 
   depends_on = [null_resource.this]
 }
